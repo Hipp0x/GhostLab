40d8a85f029702d80bb7833c5a31518a1e1f4ec0
Hipp0x <paulineadam2012@gmail.com>
2022-05-09 22:30:55 +0200

    ajout parametres pour le lancement du serveur

--- src/Serveur/Serveur.java
+++ src/Serveur/Serveur.java
@@ -2,6 +2,7 @@ package Serveur;
 
 import java.net.*;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.UnresolvedAddressException;
 import java.util.ArrayList;
 
 public class Serveur {
@@ -12,16 +13,27 @@ public class Serveur {
 
         try {
 
-            ServerSocketChannel server = ServerSocketChannel.open();
-            server.bind(new InetSocketAddress(5467));
+            if (args.length != 2) {
 
-            ServiceConnexion connexion = new ServiceConnexion(server, parties);
-            Thread t = new Thread(connexion);
-            t.start();
+                System.out.println("Veuillez entrer une adresse et un port pour le serveur au lancement de la commande.");
+                System.exit(0);
 
+            } else {
+
+                ServerSocketChannel server = ServerSocketChannel.open();
+                server.bind(new InetSocketAddress(args[0], Integer.parseInt(args[1])));
+
+                ServiceConnexion connexion = new ServiceConnexion(server, parties);
+                Thread t = new Thread(connexion);
+                t.start();
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("Veuillez rentrer un port correct pour le serveur.");
+        } catch (UnresolvedAddressException e) {
+            System.out.println("Veuillez rentrer une adresse correcte pour le serveur.");
         } catch (Exception e) {
             System.out.println(e);
-            e.printStackTrace();
         }
     }
 
